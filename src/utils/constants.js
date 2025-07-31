515ee3abbf5a661dd5fdafe28c609a886abf5d97
Kishor Hegge <kishor.h@humanfractal.io>
2025-07-31 11:47:14 +0530

    Commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,11 @@
-export const BASE_URL =
-  location.hostname === "localhost" ? "https://localhost:8000" : "/api";
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "https://localhost:8000" : "/api";
+
 
 // export const BASE_URL = "http://localhost:8000";
+
+
+export const BASE_URL =
+  typeof window !== "undefined" && window.location.hostname === "localhost"
+    ? "http://localhost:8000"
+    : "/api";
